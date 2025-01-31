3891cd6d50a7d81f512f08b9030655aaac28918d
gbcode98 <gabrielcarlossocial@gmail.com>
2025-01-31 15:06:12 -0300

    resolve error in encode

--- api/cache/cache.py
+++ api/cache/cache.py
@@ -16,7 +16,9 @@ def get_from_cache(key):
     """Tenta recuperar o cache do Redis"""
     cached_data = redis_client.get(key)
     if cached_data:
-        return cached_data.decode("utf-8")
+        if isinstance(cached_data, bytes):
+            return cached_data.decode("utf-8")  
+        return cached_data  
     return None
 
 def save_to_cache(key, value, expire=86400):
