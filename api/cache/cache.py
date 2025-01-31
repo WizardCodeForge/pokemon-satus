4ab467cf59e951ac16ee1fdb83472ac82ef7bfe9
gbcode98 <gabrielcarlossocial@gmail.com>
2025-01-31 14:32:11 -0300

    include token in code for test

--- api/cache/cache.py
+++ api/cache/cache.py
@@ -1,7 +1,15 @@
 import os
-import redis
+from upstash_redis import Redis
 
-redis_client = redis.Redis.from_url(os.getenv("KV_REST_API_URL"))
+# Se a variável de ambiente não estiver configurada, use as credenciais diretamente (não recomendado para produção)
+redis_url = os.getenv("KV_REST_API_URL")
+if not redis_url:
+    # Use as credenciais diretamente, mas tenha cuidado, pois não é seguro colocar tokens dessa forma em produção
+    redis_url = "https://key-guinea-46924.upstash.io"
+    redis_token = "AbdMAAIjcDE4NjlmNjMyNzdkMmU0MDVhYTY3NTUxODEwMjA1ZmNiNXAxMA"
+
+# Conectando ao Redis usando a URL e o token
+redis_client = Redis(url=redis_url, token=redis_token)
 
 def get_from_cache(key):
     """Tenta recuperar o cache do Redis"""
