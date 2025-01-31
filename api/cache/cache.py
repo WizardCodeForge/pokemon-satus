f8c91a31e80790b5a8ead1ae3e3767fed1981aca
gbcode98 <gabrielcarlossocial@gmail.com>
2025-01-31 14:38:33 -0300

    add upstash-redis in requirements

--- api/cache/cache.py
+++ api/cache/cache.py
@@ -1,14 +1,13 @@
 import os
 from upstash_redis import Redis
 
-# Se a variável de ambiente não estiver configurada, use as credenciais diretamente (não recomendado para produção)
+
 redis_url = os.getenv("KV_REST_API_URL")
 if not redis_url:
-    # Use as credenciais diretamente, mas tenha cuidado, pois não é seguro colocar tokens dessa forma em produção
+
     redis_url = "https://key-guinea-46924.upstash.io"
     redis_token = "AbdMAAIjcDE4NjlmNjMyNzdkMmU0MDVhYTY3NTUxODEwMjA1ZmNiNXAxMA"
 
-# Conectando ao Redis usando a URL e o token
 redis_client = Redis(url=redis_url, token=redis_token)
 
 def get_from_cache(key):
